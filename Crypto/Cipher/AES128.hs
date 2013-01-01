445992b284593abf216bc6c13ec074df4c8f93db
Thomas M. DuBuisson <thomas.dubuisson@gmail.com>
2012-12-31 22:58:49 -0800

    Don't use a newtype wrapper on the key.

--- Crypto/Cipher/AES128.hs
+++ Crypto/Cipher/AES128.hs
@@ -1,5 +1,5 @@
 module Crypto.Cipher.AES128
-  ( AES128
+  ( AESKey
   ) where
 
 import qualified Data.ByteString as B
@@ -13,10 +13,8 @@ import System.IO.Unsafe
 
 import Data.Serialize
 
-newtype AES128 = AES128 AESKey
-
-instance Serialize AES128 where
-    put (AES128 k) = do
+instance Serialize AESKey where
+    put k = do
         let RKey l h = (rawKey k)
         putWord64be h
         putWord64be l
@@ -27,20 +25,20 @@ instance Serialize AES128 where
             Nothing -> fail "Invalid key on 'get'"
             Just k  -> return k
 
-instance BlockCipher AES128 where
+instance BlockCipher AESKey where
     blockSize = Tagged 128
     keyLength = Tagged 128
     buildKey bs 
       | B.length bs >= 16 = unsafePerformIO $
           B.unsafeUseAsCString bs $ \ptr -> do
             k <- generateKey (castPtr ptr)
-            return (Just (AES128 k))
+            return (Just k)
       | otherwise = Nothing
-    encryptBlock (AES128 k) b = unsafePerformIO $ do
+    encryptBlock k b = unsafePerformIO $ do
         B.unsafeUseAsCStringLen b $ \(inP,len) -> do
          B.create (B.length b) $ \outP -> do
             encryptECB k (castPtr outP) (castPtr inP) (len`div`blkSize)
-    decryptBlock (AES128 k) b = unsafePerformIO $ do
+    decryptBlock k b = unsafePerformIO $ do
         B.unsafeUseAsCStringLen b $ \(inP,len) -> do
          B.create (B.length b) $ \outP -> do
             decryptECB k (castPtr outP) (castPtr inP) (len`div`blkSize)
