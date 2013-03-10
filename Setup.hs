85e0a92d2d6a9dd5d3b106586347929546760768
Thomas M. DuBuisson <thomas.dubuisson@gmail.com>
2013-03-10 16:09:54 -0700

    Modify Setup.hs to (correctly?) determine NI support.
    The Setup.hs system now checks in the buildHook if -maes is supported
    and, if so, set the proper flags.
    
    I could add a long rant about cabal and compiler flags not being the smoothest
    path to trot down, but don't think I would say anything surprising.

--- Setup.hs
+++ Setup.hs
@@ -1,4 +1,5 @@
 import Distribution.Simple
+import Distribution.Simple.LocalBuildInfo
 import Distribution.Simple.Setup
 import Distribution.PackageDescription
 import Distribution.Simple.Utils
@@ -7,62 +8,31 @@ import Distribution.Verbosity
 import System.Process
 import System.Directory
 import System.Exit
+import Text.Groom
 
 main = defaultMainWithHooks hk
  where
- hk = simpleUserHooks { preBuild = \as fs -> do
-                                        b <- (preBuild simpleUserHooks) as fs
-                                        checkAndAddAES fs b }
+ hk = simpleUserHooks { buildHook = \pd lbi uh bf -> do
+                                        print "yo yo yo"
+                                        let ccProg = Program "gcc" undefined undefined undefined
+                                            mConf = lookupProgram ccProg (withPrograms lbi)
+                                            err = error "Could not determine C compiler"
+                                            cc = locationPath . programLocation  . maybe err id $ mConf
+                                        b <- canUseAesIntrinsicsFlag cc
+                                        print (withPrograms lbi)
+                                        let newWithPrograms1 = userSpecifyArgs "gcc" aesArgs (withPrograms lbi)
+                                            newWithPrograms  = userSpecifyArgs "ghc" aesArgsHC newWithPrograms1
+                                        buildHook simpleUserHooks pd (lbi {withPrograms = newWithPrograms }) uh bf
+                      }
 
-checkAndAddAES :: BuildFlags -> HookedBuildInfo -> IO HookedBuildInfo
-checkAndAddAES fs x@(m,as) = do
-        b  <- canUseAesIntrinsicsFlag fs
-        b2 <- haveNIInstrs fs
-        let bi = maybe emptyBuildInfo id m
-            op = if b && b2 then addNIintrinsicOpt else id
-        return (Just (op emptyBuildInfo), as)
+aesArgs :: [String]
+aesArgs = ["-maes", "-mssse3", "-DHAVE_AES_INTRINSICS"]
 
-addNIintrinsicOpt :: BuildInfo -> BuildInfo
-addNIintrinsicOpt bi = bi { ccOptions = ccOptions bi ++ ["-maes", "-mssse3", "-DHAVE_AES_INTRINSICS", "-DHAVE_NI"] }
+aesArgsHC :: [String]
+aesArgsHC = map ("-optc" ++) aesArgs
 
--- Detect if we have AES NI instructions for acceleration on x86 CPUs
-haveNIInstrs :: BuildFlags -> IO Bool
-haveNIInstrs cf = do
-        -- FIXME cf is always empty, how do we ensure we're testing the right C compiler?
-        -- TODO: I am hooking Cabal at the wrong spot, need to hook one stage
-        -- later!
-        let prog = lookup "cc" (buildProgramPaths cf)
-            cc = maybe "gcc" id prog
-        withTempDirectory normal "" "testNI" $ \tmpDir -> do
-         writeFile (tmpDir ++ "/testNI.c")
-               (unlines ["#include <stdint.h>",
-                        "/**",
-                        " * Returns zero if false, non-zero otherwise",
-                        " */",
-                        "int cpu_has_ni()",
-                        "{",
-                        "uint32_t ax,bx,cx,dx,func=1;",
-                        "uint32_t regs[4];",
-                        "asm volatile (\"cpuid\":",
-                        "\"=a\" (ax), \"=b\" (bx), \"=c\" (cx), \"=d\" (dx) : \"a\" (func));",
-                        "return (cx & 0x2000000);",
-                        "}",
-                        "int main()",
-                        "{",
-                        "    if(cpu_has_ni()) return 0; else return -1;",
-                        "}"]
-                                )
-         ec <- rawSystemExitCode normal cc ["-maes",tmpDir ++ "/testNI.c"]
-         notice normal $ "Result of AES NI Test: " ++ show (ec == ExitSuccess)
-         return (ec == ExitSuccess)
-
-canUseAesIntrinsicsFlag :: BuildFlags -> IO Bool
-canUseAesIntrinsicsFlag cf = do
-        -- FIXME cf is always empty, how do we ensure we're testing the right C compiler?
-        -- TODO: I am hooking Cabal at the wrong spot, need to hook one stage
-        -- later!
-        let prog = lookup "cc" (buildProgramPaths cf)
-            cc = maybe "gcc" id prog
+canUseAesIntrinsicsFlag :: FilePath -> IO Bool
+canUseAesIntrinsicsFlag cc = do
         withTempDirectory normal "" "testIntrinsic" $ \tmpDir -> do
         writeFile (tmpDir ++ "/testIntrinsic.c")
                 (unlines        [ "#include <wmmintrin.h>"
