6f297595b693d05bcda65dfb370de70e180a645f
Thomas M. DuBuisson <tommd@galois.com>
2013-12-12 20:44:09 -0800

    Add KATS

---
+++ Test/test.hs
@@ -0,0 +1,9 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PackageImports #-}
+import Test.AES
+import Crypto.Cipher.AES128
+import "test-framework" Test.Framework
+
+main = do
+    ts <- makeAESTests (undefined :: AESKey)
+    defaultMain ts
