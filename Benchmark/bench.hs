0f01dad75b5f3045e34afa9bf84f9d3c1d883cf6
Thomas M. DuBuisson <tommd@galois.com>
2016-08-28 15:22:38 -0700

    Remove cipher-aes from the benchmark.

--- Benchmark/bench.hs
+++ Benchmark/bench.hs
@@ -1,5 +1,4 @@
 import qualified Crypto.Cipher.AES128 as AES128
-import qualified Crypto.Cipher.AES    as AES
 import Crypto.Classes
 import Crypto.Types
 import Criterion
@@ -17,6 +16,4 @@ main = do
     defaultMain
         [ bench "aes-ecb cipher-aes128" $ nf (AES128.encryptBlock k) pt
         , bench "aes-ctr cipher-aes128" $ nf (fst . AES128.ctr k iv) pt
-        , bench "aes-ecb cipher-aes"    $ nf (AES.encryptECB kV) pt
-        , bench "aes-ctr cipher-aes"    $ nf (AES.encryptCTR kV ivV) pt
-        , bench "aes-gcm cipher-aes"    $ nf (fst . AES.encryptGCM kV ivV B.empty) pt]
+        ]
